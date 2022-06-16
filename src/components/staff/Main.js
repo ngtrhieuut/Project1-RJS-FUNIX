7179aa5396b42cb9edecc84641a398b1bc1c4032
Nguyen Trung Hieu <ngtrhieuut@gmail.com>
2022-06-16 11:24:28 +0700

    hight level

--- src/components/staff/Main.js
+++ src/components/staff/Main.js
@@ -1,39 +1,26 @@
 import dateFormat from "dateformat";
+import { useState } from "react";
 import { STAFFS } from './staffs';
 
 function Main() {
 
-    // let numCol = 'accordion-item col-6 mt-2';
-    
-    // const handleClick2 = (e) => {
-    //     return numCol = 'accordion-item col-6 mt-2'; 
-    // }
+    const [numCol, setNumCol] = useState('accordion-item col-6 mt-2');
 
-    // const handleClick3 = (e) => {
-    //     return numCol = 'accordion-item col-4 mt-2';
-    // }
-
-    // const handleClick6 = (e) => {
-    //     return numCol = 'accordion-item col-2 mt-2';
-    // }
-
-    // console.log(numCol);
-    
     return ( 
         <div className="m-2">
             <div className="card">
                 <div className="card-header text-center fw-bold text-primary">
                     ỨNG DỤNG QUẢN LÝ NHÂN SỰ
                 </div>
-                {/* <div className="m-2">
+                <div className="m-2">
                     <p className="m-2">Chọn cột hiển thị</p>
-                    <button onClick={handleClick2}>2</button>
-                    <button onClick={handleClick3}>3</button>
-                    <button onClick={handleClick6}>6</button>
-                </div> */}
+                    <button onClick={() => setNumCol('accordion-item col-6 mt-2')}>2</button>
+                    <button onClick={() => setNumCol('accordion-item col-4 mt-2')}>3</button>
+                    <button onClick={() => setNumCol('accordion-item col-2 mt-2')}>6</button>
+                </div>
                 <div className="card-body accordion row">
                     {STAFFS.map(staff => (
-                        <div className="accordion-item col-6 mt-2" key={staff.id}>
+                        <div className={numCol} key={staff.id}>
                             <h2 className="accordion-header" id="headingOne">
                                 <button className="accordion-button fw-bold" type="button" data-bs-toggle="collapse" data-bs-target={'#collapse' + staff.id} aria-expanded="true" aria-controls="collapseOne">
                                     {staff.name}
